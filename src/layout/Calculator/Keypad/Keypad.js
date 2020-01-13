d9aefd05498a565ef56b964c122a46117aab173b
Brendan Lenzner <blenzner@uwm.edu>
2020-01-13 11:05:29 -0600

    initial commit

---
+++ src/layout/Calculator/Keypad/Keypad.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import KeypadRow from './KeypadRow/KeypadRow';
+import Button from '../../../components/Button/Button';
+
+const keypad = (props) => (
+    <section className="keypad">
+      <KeypadRow>
+        <Button onButtonPress={props.onButtonPress} type="operator">C</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">&larr;</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">%</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">/</Button>
+      </KeypadRow>
+      
+      <KeypadRow>
+        <Button onButtonPress={props.onButtonPress}>9</Button>
+        <Button onButtonPress={props.onButtonPress}>8</Button>
+        <Button onButtonPress={props.onButtonPress}>7</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">*</Button>
+      </KeypadRow>
+      
+      <KeypadRow>
+        <Button onButtonPress={props.onButtonPress}>6</Button>
+        <Button onButtonPress={props.onButtonPress}>5</Button>
+        <Button onButtonPress={props.onButtonPress}>4</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">-</Button>
+      </KeypadRow>
+      
+      <KeypadRow>
+        <Button onButtonPress={props.onButtonPress}>3</Button>
+        <Button onButtonPress={props.onButtonPress}>2</Button>
+        <Button onButtonPress={props.onButtonPress}>1</Button>
+        <Button onButtonPress={props.onButtonPress} type="operator">+</Button>
+      </KeypadRow>
+      
+      <KeypadRow>
+        <Button onButtonPress={props.onButtonPress}>0</Button>
+        <Button onButtonPress={props.onButtonPress}>.</Button>
+        <Button onButtonPress={props.onButtonPress} type="large">=</Button>
+      </KeypadRow>
+    </section>
+);
+
+export default keypad;
